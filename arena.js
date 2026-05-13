bd4af0e4ba57337d7010214f3586219e8d6491ac
gmaurel-ux <gmaurel@edenschool.fr>
2026-05-13 11:49:59 +0200

    arena js /2

--- arena.js
+++ arena.js
@@ -3,15 +3,29 @@ const Select = document.querySelector("select");
 const formData = new FormData(form);
 
 async function test() {
-	const response = await fetch("http://10.69.4.208:3000/v1/entities");
+	const response = await fetch("http://10.69.4.208:3000/v1/entities", {
+		method: "GET",
+		headers: { "content-type": "application/json" },
+		name: "",
+		image: "",
+	});
 	const json = await response.json();
+	for (let i = 0; i < json.length; i++) {
+		const option = document.createElement("option");
+		const imgentity = document.createElement("img");
+		imgentity.setAttribute("src", json[i].icon);
+		console.log(json[i].icon);
+		option.textContent = json[i].name;
 
+		option.appendChild(imgentity);
+		option.value = json[i].name;
+		Select.appendChild(option);
+	}
 	console.log(json);
 }
-
+window.addEventListener("DOMContentLoaded", () => {
+	test();
+});
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-
-	console.log(data);
-	test();
 });
